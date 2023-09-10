0b4caf79a25a84f73b2ec7339993881a7f5a4330
Nico Jensch <root@dr460nf1r3.org>
2023-09-10 15:08:08 +0200

    build(flake.nix): prevent having the same input around multiple times by specifying follows

--- flake.nix
+++ flake.nix
@@ -10,10 +10,14 @@
   inputs = {
     # The Chaotic's Nyx
     chaotic-nyx.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    chaotic-nyx.inputs.home-manager.follows = "home-manager";
 
     # Devshell to set up a development environment
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "garuda-nixpkgs";
+
+    # Common used input of our flake inputs
+    flake-utils.url = "github:numtide/flake-utils";
 
     # If you need to, override this to use a different nixpkgs version
     # by default we follow Chaotic Nyx' nyxpkgs-unstable branch
@@ -25,6 +29,8 @@
 
     # Easy linting of the flake and all kind of other stuff
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+    pre-commit-hooks.inputs.nixpkgs.follows = "garuda-nixpkgs";
 
     # NixOS hardware database
     nixos-hardware.url = "github:NixOS/nixos-hardware";
@@ -35,6 +41,7 @@
 
     # Spicetify
     spicetify-nix.url = "github:the-argus/spicetify-nix";
+    spicetify-nix.inputs.flake-utils.follows = "flake-utils";
     spicetify-nix.inputs.nixpkgs.follows = "garuda-nixpkgs";
   };
   outputs =
