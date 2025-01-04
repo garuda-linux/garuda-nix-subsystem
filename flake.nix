cf4fef3ccb8580d6b5eabb693d58626b91647ada
TNE <tne@garudalinux.org>
2025-01-04 01:01:42 +0000

    fix(flake): Remove override to fix build

--- flake.nix
+++ flake.nix
@@ -55,11 +55,9 @@
 
     # Easy linting of the flake and all kind of other stuff
     pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+      url = "github:cachix/git-hooks.nix";
       inputs.flake-compat.follows = "chaotic-nyx/nixpkgs";
       inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
-      # Only used for the tests of pre-commit-hooks. Override stops double fetch
-      inputs.nixpkgs-stable.follows = "chaotic-nyx/nixpkgs";
     };
 
     #
