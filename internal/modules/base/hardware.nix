e736ec386a6a21fa9dbad1b81b947be264d3d39f
Nico Jensch <root@dr460nf1r3.org>
2026-09-11 18:58:50 +0200

    feat(hardware): add nvidia option

--- internal/modules/base/hardware.nix
+++ internal/modules/base/hardware.nix
@@ -8,7 +8,6 @@ with garuda-lib;
 let
   cfg = config.garuda.hardware;
 in
-with garuda-lib;
 {
   options = {
     garuda.hardware.enable = lib.mkOption {
@@ -18,20 +17,40 @@ with garuda-lib;
         If set to true, reasonable defaults for hardware will be set.
       '';
     };
-  };
-
-  config = lib.mkIf cfg.enable {
-    # Microcode and firmware updates
-    hardware = {
-      cpu = {
-        amd.updateMicrocode = gDefault true;
-        intel.updateMicrocode = gDefault true;
-      };
-      enableRedistributableFirmware = gDefault true;
-      graphics = {
-        enable = gDefault config.garuda.system.isGui;
-        enable32Bit = gDefault config.garuda.system.isGui;
-      };
+    garuda.hardware.nvidia = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      example = true;
+      description = ''
+        Enable the proprietary NVIDIA driver (also bind the GPU into the
+        Garuda container via garuda.garuda-chroot.nvidia).
+      '';
     };
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      hardware = {
+        cpu = {
+          amd.updateMicrocode = gDefault true;
+          intel.updateMicrocode = gDefault true;
+        };
+        enableRedistributableFirmware = gDefault true;
+        graphics = {
+          enable = gDefault config.garuda.system.isGui;
+          enable32Bit = gDefault config.garuda.system.isGui;
+        };
+      };
+    })
+    (lib.mkIf cfg.nvidia {
+      hardware.nvidia = {
+        modesetting.enable = gDefault true;
+        open = gDefault true;
+        package = gDefault config.boot.kernelPackages.nvidiaPackages.latest;
+      };
+      services.xserver.videoDrivers = [
+        "nvidia"
+      ];
+    })
+  ];
 }
