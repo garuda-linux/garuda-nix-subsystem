49b8238139b5d93887634524a9535ad0651eda80
Nico Jensch <root@dr460nf1r3.org>
2026-09-15 19:16:04 +0200

    feat(garuda-update): support flake only systems

--- internal/modules/base/garuda-update.nix
+++ internal/modules/base/garuda-update.nix
@@ -19,6 +19,13 @@ let
         sudo "$0" "$@"
         exit 1
       fi
+
+      if [ "''${1:-}" = "--rollback" ]; then
+        echo -e "\033[1;33m-->\033[1;34m Rolling back to previous generation 🍵\033[0m"
+        nixos-rebuild switch --rollback
+        exit 0
+      fi
+
       if [ -f /etc/nixos/garuda-managed.json ]; then
         echo -e "\033[1;33m-->\033[1;34m Downloading the latest version of the updater 🍵\033[0m"
         nix run --accept-flake-config gitlab:garuda-linux/garuda-nix-subsystem/stable?dir=internal/updater#nix -- develop --refresh --accept-flake-config gitlab:garuda-linux/garuda-nix-subsystem/stable#gns-update -c "gns-update"
@@ -27,14 +34,16 @@ let
         echo -e "\033[1;33m-->\033[1;34m Updating flake inputs 🍵\033[0m"
         nix flake update --flake "$FLAKE"
         echo -e "\033[1;33m-->\033[1;34m Rebuilding system 🍵\033[0m"
-        nixos-rebuild switch --flake "$FLAKE"
+        if nixos-rebuild switch --flake "$FLAKE"; then
+          git -C "$FLAKE" add flake.lock
+          git -C "$FLAKE" -c user.name="garuda-update" -c user.email="garuda-update@localhost" commit -m "chore(flake.lock): $(date +%F)" --no-verify --quiet || true
+        fi
       fi
     '';
   };
 in
 {
   config = {
-    # This is the default updater for GNS (irrelevant for non-GNS users)
     environment.systemPackages = lib.mkIf (
       config.garuda.system.isGui || config.garuda.managed.config != null
     ) [ garuda-update ];
